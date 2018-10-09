b8634870d5ca42f8a73a666f13a2d597ee4f09b0
tornem <tornem@yandex.ru>
2018-10-09 17:22:37 +0700

    Deleted signals values in right\left arrays.

--- merge_sort.c
+++ merge_sort.c
@@ -8,8 +8,8 @@ void Merge(int* array, int left_index, int right_index, int arr_size)  // left_i
     int left_size = right_index - left_index + 1;
     int right_size = arr_size - right_index;
 
-    int* left_arr = malloc(left_size * sizeof(left_size) + 1);  // + 1 - место для маркеров максимального значения
-    int* right_arr = malloc(right_size * sizeof(right_size) + 1); 
+    int* left_arr = malloc(left_size * sizeof(left_size));  
+    int* right_arr = malloc(right_size * sizeof(right_size)); 
 
     memcpy(left_arr, array + left_index - 1, left_size * sizeof(*array));
     memcpy(right_arr, array + right_index, right_size * sizeof(*array));
@@ -24,10 +24,18 @@ void Merge(int* array, int left_index, int right_index, int arr_size)  // left_i
     //     right_arr[i] = array[right_index + i];
     // }
 
-    left_arr[left_size] = INT_MAX;  // маркер, который указывает, на конец данного массива.
-    right_arr[right_size] = INT_MAX;
+    // left_arr[left_size] = INT_MAX;  // маркер, который указывает, на конец данного массива.
+    // right_arr[right_size] = INT_MAX;
 
     for (int k = left_index - 1, i = 0, j = 0; k < arr_size; ++k) {
+        if (i == left_size) {
+            memcpy(array + k, right_arr + j, (right_size - j) * sizeof(*right_arr));
+            break;
+        }
+        if (j == right_size) {
+            memcpy(array + k, left_arr + i, (left_size - i) * sizeof(*left_arr));
+            break;
+        }
         if (left_arr[i] <= right_arr[j]) {
             array[k] = left_arr[i];
             ++i;
@@ -54,7 +62,7 @@ void MergeSort(int* array, const int left_start, const int size )
 
 int main()
 {
-    int test_arr[] = {99, 189, 0 ,-1 ,66};
+    int test_arr[] = {1, 3, 0 ,2 ,4};
     size_t size = sizeof(test_arr)/sizeof(*test_arr);
     //printf("%lu\n", sizeof(test_arr)/sizeof(*test_arr));
 
